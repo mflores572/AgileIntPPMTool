b6d33528854bdb3e8fe5abff9a2744c7ac358217
Mario Alejandro Flores <ps5mcmariof@gmail.com>
2021-12-01 09:56:23 -0600

    fixed some spacing in Project.java

--- ppmtool/src/main/java/io/agileintelligence/ppmtool/domain/Project.java
+++ ppmtool/src/main/java/io/agileintelligence/ppmtool/domain/Project.java
@@ -35,103 +35,69 @@ public class Project {
 		
 	}
 	
-	
-	
 	public Long getId() {
 		return id;
 	}
-
-
 
 	public void setId(Long id) {
 		this.id = id;
 	}
 
-
-
 	public String getProjectName() {
 		return projectName;
 	}
-
-
 
 	public void setProjectName(String projectName) {
 		this.projectName = projectName;
 	}
 
-
-
 	public String getProjectIdentifier() {
 		return projectIdentifier;
 	}
-
-
 
 	public void setProjectIdentifier(String projectIdentifier) {
 		this.projectIdentifier = projectIdentifier;
 	}
 
-
-
 	public String getDescription() {
 		return description;
 	}
-
-
 
 	public void setDescription(String description) {
 		this.description = description;
 	}
 
-
-
 	public Date getStart_date() {
 		return start_date;
 	}
-
-
 
 	public void setStart_date(Date start_date) {
 		this.start_date = start_date;
 	}
 
-
-
 	public Date getEnd_date() {
 		return end_date;
 	}
-
-
 
 	public void setEnd_date(Date end_date) {
 		this.end_date = end_date;
 	}
 
-
-
 	public Date getCreated_At() {
 		return created_At;
 	}
-
-
 
 	public void setCreated_At(Date created_At) {
 		this.created_At = created_At;
 	}
 
-
-
 	public Date getUpdated_At() {
 		return updated_At;
 	}
 
-
-
 	public void setUpdated_At(Date updated_At) {
 		this.updated_At = updated_At;
 	}
-
-
 
 	@PrePersist
 	protected void onCreate() {
@@ -142,7 +108,4 @@ public class Project {
 	protected void onUpdate() {
 		this.updated_At=new Date();
 	}
-	
-	
-
 }
