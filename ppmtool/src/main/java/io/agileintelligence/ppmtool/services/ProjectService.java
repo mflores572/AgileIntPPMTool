59065d05c68aefb626c53c5deec67ab6ddcf7e17
Mario Alejandro Flores <ps5mcmariof@gmail.com>
2021-12-03 09:06:19 -0600

    fixed/added custom error response for duplicate projectIds

--- ppmtool/src/main/java/io/agileintelligence/ppmtool/services/ProjectService.java
+++ ppmtool/src/main/java/io/agileintelligence/ppmtool/services/ProjectService.java
@@ -1,7 +1,10 @@
 package io.agileintelligence.ppmtool.services;
+
 import io.agileintelligence.ppmtool.domain.Project;
 
 import io.agileintelligence.ppmtool.repositories.ProjectRepository;
+import io.agileintelligence.ppmtool.exceptions.ProjectIdException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -10,11 +13,14 @@ public class ProjectService {
 
 	@Autowired
 	private ProjectRepository projectRepository;
-	
+
 	public Project saveOrUpdateProject(Project project) {
-		
-		//logic
-		
-		return projectRepository.save(project);
+		try {
+			project.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
+			return projectRepository.save(project);
+		} catch (Exception e) {
+			throw new ProjectIdException(
+					"Project ID '" + project.getProjectIdentifier().toUpperCase() + "' already exists.");
+		}
 	}
 }
