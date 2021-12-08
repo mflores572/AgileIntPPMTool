3c7aeac7eb91b25a16879fd82af82914ec8d695f
Mario Alejandro Flores <ps5mcmariof@gmail.com>
2021-12-08 09:36:49 -0600

    added funcitonal compopnents and react router

---
+++ ppmtool/ppmtool/src/main/java/io/agileintelligence/ppmtool/repositories/ProjectRepository.java
@@ -0,0 +1,17 @@
+package io.agileintelligence.ppmtool.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import io.agileintelligence.ppmtool.domain.Project;
+
+@Repository
+public interface ProjectRepository extends CrudRepository<Project, Long> {
+
+	//@Override
+	//Iterable<Project> findAllById(Iterable<Long> iterable);
+	Project findByProjectIdentifier(String productId);
+
+	@Override
+	Iterable<Project> findAll();
+}
