fb4bdd9a0c9dbec413d5ad25efc6e36fcb0d5195
Mario Alejandro Flores <ps5mcmariof@gmail.com>
2021-12-14 00:40:25 -0600

    grabbing individual projects for updateProject

--- ppmtool-react-client/src/actions/projectActions.js
+++ ppmtool-react-client/src/actions/projectActions.js
@@ -1,10 +1,9 @@
-import Axios from "axios";
-import axious from "axios";
-import { GET_ERRORS, GET_PROJECTS } from "./types";
+import axios from "axios";
+import { GET_ERRORS, GET_PROJECTS, GET_PROJECT } from "./types";
 
 export const createProject = (project, history) => async (dispatch) => {
   try {
-    const res = await axious.post("http://localhost:8080/api/project", project);
+    const res = await axios.post("http://localhost:8080/api/project", project);
     history.push("/dashboard");
   } catch (err) {
     dispatch({
@@ -15,9 +14,17 @@ export const createProject = (project, history) => async (dispatch) => {
 };
 
 export const getProjects = () => async (dispatch) => {
-  const res = await Axios.get("http://localhost:8080/api/project/all");
+  const res = await axios.get("http://localhost:8080/api/project/all");
   dispatch({
     type: GET_PROJECTS,
+    payload: res.data,
+  });
+};
+
+export const getProject = (id, history) => async (dispatch) => {
+  const res = await axios.get(`http://localhost:8080/api/project/${id}`);
+  dispatch({
+    type: GET_PROJECT,
     payload: res.data,
   });
 };
--- ppmtool-react-client/src/components/Project/UpdateProject.js
+++ ppmtool-react-client/src/components/Project/UpdateProject.js
@@ -1,6 +1,15 @@
 import React, { Component } from "react";
+import { getProject, createProject } from "../../actions/projectActions";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import classnames from "classnames";
 
-export default class UpdateProject extends Component {
+class UpdateProject extends Component {
+  componentDidMount() {
+    const { id } = this.props.match.params;
+    this.props.getProject(id, this.props.history);
+  }
+
   render() {
     return (
       <div className="project">
@@ -62,3 +71,14 @@ export default class UpdateProject extends Component {
     );
   }
 }
+
+UpdateProject.propTypes = {
+  getProject: PropTypes.func.isRequired,
+  project: PropTypes.object.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  project: state.project.project,
+});
+
+export default connect(mapStateToProps, { getProject })(UpdateProject);
