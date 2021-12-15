57837b47bed3d3101fb107483955aed060e3ce9e
Mario Alejandro Flores <ps5mcmariof@gmail.com>
2021-12-15 11:45:36 -0600

    Persist Project Task

--- ppmtool/src/main/java/io/agileintelligence/ppmtool/services/ProjectTaskService.java
+++ ppmtool/src/main/java/io/agileintelligence/ppmtool/services/ProjectTaskService.java
@@ -17,18 +17,37 @@ public class ProjectTaskService {
 	@Autowired
 	private ProjectTaskRepository projectTaskRepository;
 	
-	public ProjectTask addProjectTask() {
+	public ProjectTask addProjectTask(String projectIdentifier, ProjectTask projectTask) {
+		
+		//EXCEPTIONS: Project not found 
 
 		//PTs to be added to a specific project, project != null, backlog exists
-
+		Backlog backlog = backlogRepository.findByProjectIdentifier(projectIdentifier);
+		
 		//bl = backlog, pt = project tast
 		//set bl to pt
+		projectTask.setBacklog(backlog);
 		
 		//we want the sequence to be like IDPRO-1 IDPRO-2 ... 100 101
+		Integer BacklogSequence = backlog.getPTSequence();
+		
 		//Update the BL SEQUENCE
+		BacklogSequence++;
+		
+		//Add Sequence to project task
+		projectTask.setProjectSequence(projectIdentifier+"-"+BacklogSequence);
+		projectTask.setProjectIdentifier(projectIdentifier);
 		
 		//INITIAL priority when priority null
-		//INITIAL status when status is null
+		/*if(projectTask.getPriority()==0||projectTask.getPriority()==null) {
+			projectTask.setPriority(3);
+		}*/
 		
+		//INITIAL status when status is null
+		if(projectTask.getStatus()==""||projectTask.getStatus()==null) {
+			projectTask.setStatus("TO_DO");
+		}
+		
+		return projectTaskRepository.save(projectTask);
 	}
 }
