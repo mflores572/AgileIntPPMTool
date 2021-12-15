677661a85c9ce8ee91481d37c8295c380612944e
Mario Alejandro Flores <ps5mcmariof@gmail.com>
2021-12-15 11:55:50 -0600

    bug fixing ProjectTask priority, projectIdentifier, PTSequence

--- ppmtool/src/main/java/io/agileintelligence/ppmtool/services/ProjectTaskService.java
+++ ppmtool/src/main/java/io/agileintelligence/ppmtool/services/ProjectTaskService.java
@@ -33,15 +33,16 @@ public class ProjectTaskService {
 		
 		//Update the BL SEQUENCE
 		BacklogSequence++;
+		backlog.setPTSequence(BacklogSequence);
 		
 		//Add Sequence to project task
 		projectTask.setProjectSequence(projectIdentifier+"-"+BacklogSequence);
 		projectTask.setProjectIdentifier(projectIdentifier);
 		
 		//INITIAL priority when priority null
-		/*if(projectTask.getPriority()==0||projectTask.getPriority()==null) {
+		if(projectTask.getPriority() == null) {//In the future we need to "projectTask.getPriority()==0" to handle the form
 			projectTask.setPriority(3);
-		}*/
+		}
 		
 		//INITIAL status when status is null
 		if(projectTask.getStatus()==""||projectTask.getStatus()==null) {
