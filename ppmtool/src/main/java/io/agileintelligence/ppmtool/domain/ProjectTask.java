16726381587931d5f5c14ec21b4ade534073f909
Mario Alejandro Flores <ps5mcmariof@gmail.com>
2021-12-16 00:37:33 -0600

    delete operation, improved backlog/project task relationship (bug fi)

--- ppmtool/src/main/java/io/agileintelligence/ppmtool/domain/ProjectTask.java
+++ ppmtool/src/main/java/io/agileintelligence/ppmtool/domain/ProjectTask.java
@@ -23,7 +23,7 @@ public class ProjectTask {
 	private Date dueDate;
 	
 	// ManyToOne with Backlog
-	@ManyToOne(fetch = FetchType.EAGER, cascade = CascadeType.REFRESH)
+	@ManyToOne(fetch = FetchType.EAGER) //REMOVE REFRESH
 	@JoinColumn(name = "backlog_id", updatable = false, nullable = false)
 	@JsonIgnore
 	private Backlog backlog;
--- ppmtool/src/main/java/io/agileintelligence/ppmtool/services/ProjectTaskService.java
+++ ppmtool/src/main/java/io/agileintelligence/ppmtool/services/ProjectTaskService.java
@@ -108,11 +108,6 @@ public class ProjectTaskService {
 	public void deletePTByProjectSequence(String backlog_id, String pt_id) {
 		ProjectTask projectTask = findPTByProjectSequence(backlog_id, pt_id);
 		
-		Backlog backlog = projectTask.getBacklog();
-		List<ProjectTask> pts = backlog.getProjectTasks();
-		pts.remove(projectTask);
-		backlogRepository.save(backlog);
-		
 		projectTaskRepository.delete(projectTask);
 	}
 	//Update project task
