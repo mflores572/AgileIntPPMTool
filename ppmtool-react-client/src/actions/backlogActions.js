7d12365948b398c363bdc3a0634a4fa2ac849d99
Mario Alejandro Flores <ps5mcmariof@gmail.com>
2021-12-22 13:08:46 -0600

    Update Project task part 3 Handled erros

--- ppmtool-react-client/src/actions/backlogActions.js
+++ ppmtool-react-client/src/actions/backlogActions.js
@@ -6,6 +6,10 @@ export const addProjectTask =
     try {
       await axios.post(`/api/backlog/${backlog_id}`, project_task);
       history.push(`/projectBoard/${backlog_id}`);
+      dispatch({
+        type: GET_ERRORS,
+        payload: {},
+      });
     } catch (err) {
       dispatch({
         type: GET_ERRORS,
@@ -39,5 +43,22 @@ export const getProjectTask =
       });
     } catch (err) {
       history.push("/dashboard");
+    }
+  };
+
+export const updateProjectTask =
+  (backlog_id, pt_id, project_task, history) => async (dispatch) => {
+    try {
+      await axios.patch(`/api/backlog/${backlog_id}/${pt_id}`, project_task);
+      history.push(`/projectBoard/${backlog_id}`);
+      dispatch({
+        type: GET_ERRORS,
+        payload: {},
+      });
+    } catch (err) {
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data,
+      });
     }
   };
